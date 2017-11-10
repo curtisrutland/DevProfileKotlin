a8b68c9ed5c666a92b3ac517f9a1398c23557b35
Curtis Rutland <curtisrutland@gmail.com>
2017-11-10 12:21:54 -0600

    working phone portrait layout

--- app/src/main/java/com/curtisrutland/devprofile/MainActivity.kt
+++ app/src/main/java/com/curtisrutland/devprofile/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.curtisrutland.devprofile
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bmp = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bmp)
+        rounded.cornerRadius = 15f
+        //rounded.isCircular = true
+        logo.setImageDrawable(rounded)
     }
 }
